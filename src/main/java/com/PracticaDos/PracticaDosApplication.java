90a0e5f889420c1647d4787eca9c3a179eb67d6e
vquiros1996 <viviqm127@hotmail.com>
2023-10-03 16:46:58 -0600

    Finalizacion de tarea con mis cambios, agregando a repositorio para compartir

---
+++ src/main/java/com/PracticaDos/PracticaDosApplication.java
@@ -0,0 +1,13 @@
+package com.PracticaDos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaDosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaDosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PracticaDos/PracticaDosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PracticaDos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaDosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
